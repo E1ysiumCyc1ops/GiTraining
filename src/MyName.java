32c9a05f80a961cc3a5084dceab6553ad71b5056
leo-capvano <leocapuano0@gmail.com>
2020-11-10 22:34:47 +0100

    commit by me

--- src/MyName.java
+++ src/MyName.java
@@ -1,10 +1,7 @@
 class MyName {
 
     public static void main(String[] args) {
-        System.out.println("Nicola Serra");
-
-        System.out.println("Leo");
-        System.out.println("Christian Nocerino");
+        System.out.println("@leo.capvano");
     }
 
 }

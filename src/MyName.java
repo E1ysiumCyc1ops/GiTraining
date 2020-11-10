a9904731fec4d1420b9211c3c4bfccbdb818b29d
SerNick18 <nicola.serra@hotmail.it>
2020-11-10 20:51:08 +0100

    Inserimento nome

--- src/MyName.java
+++ src/MyName.java
@@ -1,7 +1,8 @@
 class MyName {
 
     public static void main(String[] args) {
-        System.out.println("Nicola Serr");
+        System.out.println("Nicola Serra");
+
         System.out.println("Leo");
         System.out.println("Christian Nocerino");
     }

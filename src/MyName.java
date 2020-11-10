6b73d5b610bdb3ea3713b6549a62d1762414e623
SerNick18 <nicola.serra@hotmail.it>
2020-11-10 22:28:38 +0100

    prova

--- src/MyName.java
+++ src/MyName.java
@@ -1,10 +1,7 @@
 class MyName {
 
     public static void main(String[] args) {
-        System.out.println("Eugenio");
-        System.out.println("Ciaoooo");
         System.out.println("Nicola Serra");
-
         System.out.println("Leo");
         System.out.println("Christian Nocerino");
     }

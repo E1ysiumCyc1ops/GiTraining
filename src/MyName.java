8d8538dd5960bc5135254a29de12596450b8b3aa
p.napolitano18@studenti.unisa.it <Quadrelle1>
2020-11-22 13:00:37 +0100

    Ho tolto gli altri nomi e ho lasciato solo la stampa del mio

--- src/MyName.java
+++ src/MyName.java
@@ -1,13 +1,6 @@
 class MyName {
 
     public static void main(String[] args) {
-        System.out.println("Nicola Serra");
-        System.out.println("Nicola-->Prova1");
-        System.out.println("Leo");
-        System.out.println("Christian Nocerino");
-        System.out.println("Nicola-->Prova2");
         System.out.println("Pasquale Napolitano");
-        System.out.println("Prova4");
     }
-
 }
